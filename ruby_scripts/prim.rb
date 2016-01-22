a39de15d1b9fae94864d15dfc61e2e95b7f41e60
hueby <dennis.huebner89gmail.com>
2016-01-22 13:38:46 +0100

    refinements and counting stuff addded :)

--- ruby_scripts/prim.rb
+++ ruby_scripts/prim.rb
@@ -1,12 +1,26 @@
-def prim value
-  res = [] 
-  if value <= 1
-    return 1
-  end
+#  description
+#  try all values between 2..value - 1
+#  and collect all modulos in a set
+#  if theres a zero in this set,
+#  the value is no prime number
 
+def prim value
   for i in 2..(value - 1)
-      res.push(value % i)
+    if value % i == 0 
+      return 0
     end
-     
-  res.min != 0 ? 1 : 0
+  end
+  
+  return 1
+end
+
+def getprimto max
+  primes = []
+  for i in 1..max  
+    prime = prim(i)
+    if prime == 1
+      primes.push(i)
+    end
+  end
+  printf('There are %d prime numbers', primes.count)
 end
